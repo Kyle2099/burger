ebb0884b3f9b2d14419bbe336599178dcdecba9f
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-06-27 16:32:16 -0500

    file set up

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql")
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgerdb2",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+})
+
+connection.connect(function (err) {
+    if (err) {
+        console.log("we got an error!" + err.stack)
+        return
+    };
+    console.log("connected as ID " + connection.threadId)
+})
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Port = process.env.Port || 3000;
+
+const app = express()
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+const expressHandlebars = require("express-handlebars");
+app.engine("handlebars", expressHandlebars({defaultLayout: "main"}))
+
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgercontroler.js")
+app.use(routes)
+
+app.listen(Port, ()=> console.log("app listening on Port 3000!"));
+
