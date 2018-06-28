017c74afb20afa88515d1c0166fbc4ea62ea110d
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-06-28 14:52:03 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql")
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgersdb2",
-    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+    host: "o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "jveftc5thv2gyvo3",
+    password: "y2rcd5glqt46mtf4",
+    database: "vyrumsohuyhaoltl"
+    // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
 })
 
 connection.connect(function (err) {
